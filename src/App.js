f9b81cc44eeb803371c7f3613f7a938b1c646fc6
Devadath-pb <devadathpb98@gmail.com>
2024-12-28 21:27:46 +0530

    product app made with react

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import ProdectDetail from './componets/ProdectDetail';
+import Search from './componets/Search';
+import Display from './componets/Display';
+
+function App() {
+  return (
+    <div >
+        <ProdectDetail />
+        <Search />
+        <Display />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componets/Display.jsx
@@ -0,0 +1,142 @@
+import React from 'react'
+
+const Display = () => {
+    return (
+        <div>
+
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                                <div class="card" >
+                                    <img src="https://m.media-amazon.com/images/I/71DozMyPCBL.jpg" class="card-img-top" alt="..."></img>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Labtob</h5>
+                                            <p class="card-text">₹79,990.00 INR</p>
+                                            <a href="#" class="btn btn-primary">Buy now</a>
+                                        </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                            <div class="card" >
+                                    <img src="https://m.media-amazon.com/images/I/71DozMyPCBL.jpg" class="card-img-top" alt="..."></img>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Labtob</h5>
+                                            <p class="card-text">₹79,990.00 INR</p>
+                                            <a href="#" class="btn btn-primary">Buy now</a>
+                                        </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                            <div class="card" >
+                                    <img src="https://m.media-amazon.com/images/I/71DozMyPCBL.jpg" class="card-img-top" alt="..."></img>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Labtob</h5>
+                                            <p class="card-text">₹79,990.00 INR</p>
+                                            <a href="#" class="btn btn-primary">Buy now</a>
+                                        </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                            <div class="card" >
+                                    <img src="https://m.media-amazon.com/images/I/71DozMyPCBL.jpg" class="card-img-top" alt="..."></img>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Labtob</h5>
+                                            <p class="card-text">₹79,990.00 INR</p>
+                                            <a href="#" class="btn btn-primary">Buy now</a>
+                                        </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                            <div class="card" >
+                                    <img src="https://m.media-amazon.com/images/I/71DozMyPCBL.jpg" class="card-img-top" alt="..."></img>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Labtob</h5>
+                                            <p class="card-text">₹79,990.00 INR</p>
+                                            <a href="#" class="btn btn-primary">Buy now</a>
+                                        </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                            <div class="card" >
+                                    <img src="https://m.media-amazon.com/images/I/71DozMyPCBL.jpg" class="card-img-top" alt="..."></img>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Labtob</h5>
+                                            <p class="card-text">₹79,990.00 INR</p>
+                                            <a href="#" class="btn btn-primary">Buy now</a>
+                                        </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                            <div class="card" >
+                                    <img src="https://m.media-amazon.com/images/I/71DozMyPCBL.jpg" class="card-img-top" alt="..."></img>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Labtob</h5>
+                                            <p class="card-text">₹79,990.00 INR</p>
+                                            <a href="#" class="btn btn-primary">Buy now</a>
+                                        </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                            <div class="card" >
+                                    <img src="https://m.media-amazon.com/images/I/71DozMyPCBL.jpg" class="card-img-top" alt="..."></img>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Labtob</h5>
+                                            <p class="card-text">₹79,990.00 INR</p>
+                                            <a href="#" class="btn btn-primary">Buy now</a>
+                                        </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                            <div class="card" >
+                                    <img src="https://m.media-amazon.com/images/I/71DozMyPCBL.jpg" class="card-img-top" alt="..."></img>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Labtob</h5>
+                                            <p class="card-text">₹79,990.00 INR</p>
+                                            <a href="#" class="btn btn-primary">Buy now</a>
+                                        </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                            <div class="card" >
+                                    <img src="https://m.media-amazon.com/images/I/71DozMyPCBL.jpg" class="card-img-top" alt="..."></img>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Labtob</h5>
+                                            <p class="card-text">₹79,990.00 INR</p>
+                                            <a href="#" class="btn btn-primary">Buy now</a>
+                                        </div>
+                                </div>
+
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Display
